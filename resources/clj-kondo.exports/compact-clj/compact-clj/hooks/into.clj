3d7840cd680907dc9022973a5ac906f5e4070c38
Sebastian Andersen <5475129+lambdank@users.noreply.github.com>
2023-08-25 11:30:36 +0200

    Refactored into compressions

--- resources/clj-kondo.exports/compact-clj/compact-clj/hooks/into.clj
+++ resources/clj-kondo.exports/compact-clj/compact-clj/hooks/into.clj
@@ -1,24 +1,25 @@
 (ns ^:no-doc hooks.into
   (:require
-   [clj-kondo.hooks-api :as api]
    [hooks.utils :as u]))
 
-(defn into->vec [node]
-  (let [{[$into $to $from] :children} node]
-    (when (and (every-pred u/empty? u/vector?) $to)
-      (api/reg-finding!
-       (merge (meta $into)
-              {:message (u/->msg node (str "(vec " $from ")"))
-               :type :lol})))))
+(defn- legal? [node]
+  ;; Only compressions for (into to from) currently
+  (u/count? node 3))
 
-(defn into->set [node]
-  (let [{[$into $to $from] :children} node]
-    (when ((every-pred u/empty? u/set?) $to)
-      (api/reg-finding!
-       (merge (meta $into)
-              {:message (u/->msg node (str "(set " $from ")"))
-               :type :lol})))))
+(defn into->vec
+  "Compression: (into [] [1 2 3])"
+  [{:keys [children] :as node}]
+  (let [[$into $to $from] children]
+    (when (and (every-pred u/empty? u/vector?) $to)
+      (u/reg-compression! node $into (str "(vec " $from ")")))))
+
+(defn into->set
+  "Compression: (into #{} [1 2 3])"
+  [{:keys [children] :as node}]
+  (let [[$into $to $from] children]
+    (when (and (every-pred u/empty? u/set?) $to)
+      (u/reg-compression! node $into (str "(set " $from ")")))))
 
 (defn all [{:keys [node]}]
-  (when (u/in-source? node)
+  (when (and (u/in-source? node) (legal? node))
     ((juxt into->set into->vec) node)))
