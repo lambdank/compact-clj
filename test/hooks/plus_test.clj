2ea8703cddd019fd6c2ec9bb60cde3f55bd3dceb
Sebastian Andersen <5475129+lambdank@users.noreply.github.com>
2023-08-30 10:08:19 +0200

    Property tested plus (+)

--- test/hooks/plus_test.clj
+++ test/hooks/plus_test.clj
@@ -2,6 +2,8 @@
   (:require
    [clj-kondo.hooks-api :as api]
    [clojure.test :refer [deftest use-fixtures is]]
+   [clojure.test.check :as tc]
+   [clojure.test.check.properties :as prop]
    [hooks.plus]
    [hooks.test-utils :as tu]))
 
@@ -14,10 +16,26 @@
          (->> (-> #'hooks.plus/+-remove-nested meta :example :in str api/parse-string hooks.plus/+-remove-nested)
               (map #(dissoc % :highlight-node))))))
 
+(def +->inc-equivalent-property
+  (prop/for-all [x (tu/generator [:or
+                                  [:int]
+                                  [:double {:gen/NaN? false}]])]
+                (let [input (str `(+ ~x 1))]
+                  (tu/equivalent? hooks.plus/+->inc input))))
+
 (deftest +->inc-test
   (tu/test-example! #'hooks.plus/+->inc)
-  (tu/test-example! #'hooks.plus/+->inc {:in '(+ 1 n)}))
+  (tu/test-example! #'hooks.plus/+->inc {:in '(+ 1 n)})
+  (is (:pass? (tc/quick-check 100 +->inc-equivalent-property))))
+
+(def +->dec-equivalent-property
+  (prop/for-all [x (tu/generator [:or
+                                  [:int]
+                                  [:double {:gen/NaN? false}]])]
+                (let [input (str `(+ ~x -1))]
+                  (tu/equivalent? hooks.plus/+->dec input))))
 
 (deftest +->dec-test
   (tu/test-example! #'hooks.plus/+->dec)
-  (tu/test-example! #'hooks.plus/+->dec {:in '(+ -1 n)}))
+  (tu/test-example! #'hooks.plus/+->dec {:in '(+ -1 n)})
+  (is (:pass? (tc/quick-check 100 +->dec-equivalent-property))))
