e3e4bc7e434f02ef06a32fe3a1c5b4855c7d7620
Sebastian Andersen <5475129+lambdank@users.noreply.github.com>
2023-09-05 09:03:21 +0200

    Property tested into

--- test/hooks/into_test.clj
+++ test/hooks/into_test.clj
@@ -1,13 +1,27 @@
 (ns hooks.into-test
   (:require
-   [clojure.test :refer [deftest use-fixtures]]
+   [clojure.test :refer [deftest use-fixtures is]]
+   [clojure.test.check :as tc]
+   [clojure.test.check.properties :as prop]
    [hooks.into]
    [hooks.test-utils :as tu]))
 
 (use-fixtures :once tu/mock-reg-compression)
 
+(def into->set-equivalent-property
+  (prop/for-all [coll (tu/generator ::tu/seq-not-symbol)]
+                (let [input (str `(~'into #{} ~coll))]
+                  (tu/equivalent? hooks.into/into->set input))))
+
 (deftest into->set-test
-  (tu/test-example! #'hooks.into/into->set))
+  (tu/test-example! #'hooks.into/into->set)
+  (is (:pass? (tc/quick-check 100 into->set-equivalent-property))))
+
+(def into->vec-equivalent-property
+  (prop/for-all [coll (tu/generator ::tu/seq-not-symbol)]
+                (let [input (str `(~'into [] ~coll))]
+                  (tu/equivalent? hooks.into/into->vec input))))
 
 (deftest into->vec-test
-  (tu/test-example! #'hooks.into/into->vec))
+  (tu/test-example! #'hooks.into/into->vec)
+  (is (:pass? (tc/quick-check 100 into->vec-equivalent-property))))
