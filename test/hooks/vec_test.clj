552293620b9136257b7cd4415d1e27aa1d124fa4
Sebastian Andersen <5475129+lambdank@users.noreply.github.com>
2023-09-05 08:32:22 +0200

    Property tested vec

--- test/hooks/vec_test.clj
+++ test/hooks/vec_test.clj
@@ -1,13 +1,33 @@
 (ns hooks.vec-test
   (:require
-   [clojure.test :refer [deftest use-fixtures]]
+   [clojure.test :refer [deftest use-fixtures is]]
+   [clojure.test.check :as tc]
+   [clojure.test.check.properties :as prop]
    [hooks.test-utils :as tu]
    [hooks.vec]))
 
 (use-fixtures :once tu/mock-reg-compression)
 
+(def vec->mapv-equivalent-property
+  (prop/for-all [f (tu/generator [:or
+                                  [:= 'identity]
+                                  [:= 'not]])
+                 coll (tu/generator ::tu/seq-not-symbol)]
+                (let [input (str `(~'vec (~'map ~f ~coll)))]
+                  (tu/equivalent? hooks.vec/vec->mapv input))))
+
 (deftest vec->mapv-test
-  (tu/test-example! #'hooks.vec/vec->mapv))
+  (tu/test-example! #'hooks.vec/vec->mapv)
+  (is (:pass? (tc/quick-check 100 vec->mapv-equivalent-property))))
+
+(def vec->filterv-equivalent-property
+  (prop/for-all [f (tu/generator [:or
+                                  [:= 'identity]
+                                  [:= 'not]])
+                 coll (tu/generator ::tu/seq-not-symbol)]
+                (let [input (str `(~'vec (~'filter ~f ~coll)))]
+                  (tu/equivalent? hooks.vec/vec->filterv input))))
 
 (deftest vec->filterv-test
-  (tu/test-example! #'hooks.vec/vec->filterv))
+  (tu/test-example! #'hooks.vec/vec->filterv)
+  (is (:pass? (tc/quick-check 100 vec->filterv-equivalent-property))))
